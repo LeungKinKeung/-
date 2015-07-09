97c7701ca8aad26382c10628e2527c596e4ac14a
Apple <apple@MacBook-Pro.local>
2015-07-09 21:58:21 +0800

    Initial Commit

---
+++ 碰撞检测/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  碰撞检测
+//
+//  Created by Apple on 15/7/9.
+//  Copyright (c) 2015年 LKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 碰撞检测/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  碰撞检测
+//
+//  Created by Apple on 15/7/9.
+//  Copyright (c) 2015年 LKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
